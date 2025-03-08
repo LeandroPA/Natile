796f4d3bb629d9cce592d71937c713437fa8a18a
Leandro Pereira <leandroalencar@unigranrio.br>
2025-03-08 13:02:12 -0300

    feat: adicionando mapa de cobertura

--- themes/default/scripts.js
+++ themes/default/scripts.js
@@ -30,4 +30,36 @@ function main() {
   // setListener(document.getElementsByClassName("background-overlay"), "click", onClickOutsideCollapsibleButton);
 }
 
+const coberturaMap = [
+  {
+      center: { lat : -22.896373, lng: -43.560605},
+      radius: 3000
+  }
+];
+
+function initMap() {
+  console.log("init map")
+  const map = new google.maps.Map(document.getElementById("cobertura-map"), {
+      zoom: 13,
+      center: { lat : -22.896373, lng: -43.560605},
+  });
+
+  for (const cobertura of coberturaMap) {
+    
+    console.log("init cobertura", cobertura)
+      const coberturaCircle = new google.maps.Circle({
+          strokeColor: "#F97302",
+          strokeOpacity: 0.8,
+          strokeWeight: 2,
+          fillColor: "#F97302",
+          fillOpacity: 0.35,
+          map,
+          center: cobertura.center,
+          radius: cobertura.radius,
+      });
+  }
+}
+
+window.initMap = initMap;
+
 main();
