2205e12ffb6d9c90ea69c0315179f974484ddb72
Leandro Pereira <leandroalencar@unigranrio.br>
2025-03-12 11:26:54 -0300

    feat: makefile

---
+++ Makefile
@@ -0,0 +1,2 @@
+run:
+	/c/Ruby27-x64/bin/bundle.cmd exec ruby "./scaffold.rb"
--- themes/default/scripts.js
+++ themes/default/scripts.js
@@ -12,7 +12,7 @@ function toggleCollapsibleButton(element) {
 		buttonToggling = false;
 		$('.collapsible-active').each(function() {
 			this.scrollIntoView({behavior: 'smooth'});
-		});
+		});	
 	});
 }
 
